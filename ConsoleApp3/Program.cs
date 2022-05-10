c24a4743c9d8603fdd6a598e3569538c09398bdf
DrSteave <dr.steave@icloud.com>
2022-05-10 23:17:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите цвет с маленькой буквы");
+
+            switch (Console.ReadLine())
+            {
+                case "red":
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Ваш цвет красный!");
+                    return;
+
+                case "green":
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Ваш цвет зеленый!");
+                    return;
+
+                case "cyan":
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Ваш цвет голубой!");
+                    break;
+
+
+                default:
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Ваш цвет хз какой. Вы -- идиот.");
+                    return;
+
+            }
+
+
+        }
+    }
+}
